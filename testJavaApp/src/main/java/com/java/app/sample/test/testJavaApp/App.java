e7bb4e1566a15cd1fdb531043f86bf934690a36c
raivenkat <venkey.bigdata@gmail.com>
2020-05-01 18:49:13 +0530

    FirstCommit

---
+++ testJavaApp/src/main/java/com/java/app/sample/test/testJavaApp/App.java
@@ -0,0 +1,13 @@
+package com.java.app.sample.test.testJavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
